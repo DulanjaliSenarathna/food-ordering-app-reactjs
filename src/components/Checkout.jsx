3105b2cd2be28269badb0c1fe998cd146aaf804b
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2024-02-12 10:36:05 +0530

    Handling Form Submission & Validation

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -16,10 +16,15 @@ export default function Checkout(){
     userProgressCtx.hideCheckout();
   }
 
- 
+ function handleSubmit(event){
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+ }
 
     return <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Checkout</h2>
             <p>Total Amount : {currencyFormatter.format(cartTotal)}</p>
             <Input label='Full Name' type='text' id='full-name'/>
